da790594a358b47154f58358a70c89cbaf617919
Carlos Jaramillo <cejaramillof@gmail.com>
2021-01-09 18:53:52 -0500

    add bands project with sockets

---
+++ src/bands/App.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+import { BandAdd } from './componets/BandAdd';
+import { BandList } from './componets/BandList';
+
+const connectSocketServer = () => io.connect('http://localhost:8080', {
+  transports: ['websocket']
+})
+
+function App() {
+  const [socket] = useState(connectSocketServer())
+  const [online, setOnline] = useState(false);
+  const [bands, setBands] = useState([]);
+
+  useEffect(() => {
+    setOnline(socket.connected);
+    // return socket.disconnect();
+  }, [socket])
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setOnline(true);
+    })
+  }, [socket])
+
+  useEffect(() => {
+    socket.on('disconnect', () => {
+      setOnline(false);
+    })
+  }, [socket])
+
+  useEffect(() => {
+    socket.on('current-bands', (bands) => {
+      setBands(bands);
+    })
+  }, [socket])
+
+  const votar = (id) => {
+    socket.emit('votar-banda', id);
+  }
+
+  const borrar = (id) => {
+    socket.emit('borrar-banda', id);
+  }
+
+  const cambiarNombre = (id, nombre) => {
+    socket.emit('cambiar-nombre-banda', { id, nombre });
+  }
+
+  const crearBanda = (nombre) => {
+    socket.emit('crear-banda', { nombre });
+  }
+
+  return (
+    <div className="container">
+      <div className="alert">
+        <p>
+          Service status:
+          {
+            online
+              ? <span className="text-success"> Online</span>
+              : <span className="text-danger"> Offline</span>
+          }
+        </p>
+      </div>
+      <h1>BandNames</h1>
+      <hr />
+      <div className="row">
+        <div className="col-8">
+          <BandList
+            data={bands}
+            votar={votar}
+            borrar={borrar}
+            cambiarNombre={cambiarNombre}
+          />
+        </div>
+        <div className="col-4">
+          <BandAdd
+            crearBanda={crearBanda}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
